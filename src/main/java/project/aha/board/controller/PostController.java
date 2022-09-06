8cead09cf6ab9bb7c222ae1e11aa6578a7044edb
bestinwoo <bestinwoo@gmail.com>
2022-09-06 23:36:15 +0900

    게시글 API Swagger 연동

--- src/main/java/project/aha/board/controller/PostController.java
+++ src/main/java/project/aha/board/controller/PostController.java
@@ -1,5 +1,7 @@
 package project.aha.board.controller;
 
+import static org.springframework.http.MediaType.*;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,6 +24,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import project.aha.board.dto.PostDto;
 import project.aha.board.service.PostService;
@@ -30,6 +40,7 @@ import project.aha.common.dto.ErrorResponse;
 import project.aha.common.dto.Result;
 import project.aha.common.validation.ValidationSequence;
 
+@Tag(name = "post", description = "게시글 API")
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin()
@@ -37,6 +48,11 @@ import project.aha.common.validation.ValidationSequence;
 public class PostController {
 	private final PostService postService;
 
+	@Operation(summary = "게시글 작성")
+	@ApiResponses({
+		@ApiResponse(responseCode = "201", description = "CREATED", content = @Content(schema = @Schema(implementation = Result.class), mediaType = APPLICATION_JSON_VALUE)),
+		@ApiResponse(responseCode = "400", description = "BAD REQUEST", content = @Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = APPLICATION_JSON_VALUE))
+	})
 	@PostMapping("/post")
 	public ResponseEntity<BasicResponse> writePost(@Validated(ValidationSequence.class) PostDto.Request postDto,
 		@RequestParam("file") MultipartFile file) {
@@ -48,6 +64,13 @@ public class PostController {
 		}
 	}
 
+	@Operation(summary = "게시글 목록 조회")
+	@ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = PostDto.Response.class), mediaType = APPLICATION_JSON_VALUE))
+	@Parameters({
+		@Parameter(name = "boardId", description = "게시판 ID"),
+		@Parameter(name = "keyword", description = "검색어"),
+		@Parameter(name = "tagName", description = "태그 검색어")
+	})
 	@GetMapping("/post")
 	public ResponseEntity<BasicResponse> getPost(Pageable pageable, @RequestParam Long boardId,
 		@RequestParam(required = false, defaultValue = "") String keyword,
@@ -63,11 +86,16 @@ public class PostController {
 		return ResponseEntity.ok(new Result<>(posts, posts.getNumberOfElements()));
 	}
 
+	@Operation(summary = "게시글 상세 조회")
+	@Parameter(name = "postId", description = "게시글 ID")
+	@ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = PostDto.Response.class)))
 	@GetMapping("/post/{postId}")
 	public ResponseEntity<BasicResponse> getPostDetail(@PathVariable Long postId) {
 		return ResponseEntity.ok(new Result<>(postService.getPostDetail(postId)));
 	}
 
+	@Operation(summary = "게시글 수정")
+	@Parameter(name = "postId", description = "게시글 ID")
 	@PutMapping("/post/{postId}")
 	public ResponseEntity<BasicResponse> modifyPost(@PathVariable Long postId, @RequestBody PostDto.Request request) {
 		try {
@@ -78,55 +106,11 @@ public class PostController {
 		}
 	}
 
+	@Operation(summary = "게시글 삭제", description = "작성자만 삭제가 가능하다.")
+	@Parameter(name = "postId", description = "게시글 ID")
 	@DeleteMapping("/post/{postId}")
 	public ResponseEntity<BasicResponse> deletePost(@PathVariable Long postId) {
 		postService.deletePost(postId);
 		return ResponseEntity.ok().build();
 	}
-
-	// @PatchMapping("/post/{postId}")
-	// public ResponseEntity<BasicResponse> modifyPost(@PathVariable Long postId, @RequestBody PostDto postDto) {
-	// 	if (!validateUser(postId)) {
-	// 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-	// 			.body(new ErrorResponse("작성자만 게시글을 삭제할 수 있습니다.", "401"));
-	// 	}
-	//
-	// 	if (postService.modifyPost(postId, postDto) == 0) {
-	// 		return ResponseEntity.notFound().build();
-	// 	}
-	//
-	// 	return ResponseEntity.noContent().build();
-	// }
-	//
-	//
-	// @GetMapping("/post/{boardId}/{postId}")
-	// public ResponseEntity<BasicResponse> getPostDetail(@PathVariable Long boardId, @PathVariable Long postId) {
-	// 	if (postService.increaseViews(postId) == 0) {
-	// 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse("게시글을 찾을 수 없습니다."));
-	// 	}
-	// 	return ResponseEntity.ok(new Result<>(postService.postDetail(postId)));
-	// }
-	//
-	// @DeleteMapping("/post/{postId}")
-	// public ResponseEntity<BasicResponse> deletePost(@PathVariable Long postId) {
-	// 	if (!validateUser(postId)) {
-	// 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-	// 			.body(new ErrorResponse("작성자만 게시글을 삭제할 수 있습니다.", "401"));
-	// 	}
-	//
-	// 	if (postService.deletePost(postId) == 0) {
-	// 		return ResponseEntity.notFound().build();
-	// 	}
-	// 	return ResponseEntity.noContent().build();
-	// }
-	//
-	// private boolean validateUser(Long postId) {
-	// 	Long currentUserId = SecurityUtil.getCurrentMemberId();
-	// 	Long writerId = postService.postDetail(postId).getUserId();
-	// 	if (currentUserId != writerId) {
-	// 		return false;
-	// 	}
-	// 	return true;
-	// }
-
 }
